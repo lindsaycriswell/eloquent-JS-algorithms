f484b020b705b488417156801f84feb5b19b88ab
lindsaycriswell <lindsaycriswell@gmail.com>
2018-05-07 12:41:11 -0400

    minimum

---
+++ minimum.js
@@ -0,0 +1,14 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(x, y) {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
