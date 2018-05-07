99ef54d7b2cdfcc1c2c906ced67faf14ce61499c
lindsaycriswell <lindsaycriswell@gmail.com>
2018-05-07 13:15:08 -0400

    reversing-an-array

--- reversing-an-array.js
+++ reversing-an-array.js
@@ -8,3 +8,27 @@
 // Thinking back to the notes about side effects and pure functions in the
 // previous chapter, which variant do you expect to be useful in more situations?
 // Which one runs faster?
+
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
